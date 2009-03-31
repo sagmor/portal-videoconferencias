5eb1fb5479ea787948d99e8b7242ce8ae16e737e
Sebastián Gamboa (SagMor) <me@sagmor.com>
2009-03-30 23:47:34 -0400

    Initial Commit

---
+++ cake/config/config.php
@@ -0,0 +1,26 @@
+<?php
+/* SVN FILE: $Id: config.php 8120 2009-03-19 20:25:10Z gwoo $ */
+/**
+ * Core Configurations.
+ *
+ * PHP versions 4 and 5
+ *
+ * CakePHP(tm) :  Rapid Development Framework (http://www.cakephp.org)
+ * Copyright 2005-2008, Cake Software Foundation, Inc. (http://www.cakefoundation.org)
+ *
+ * Licensed under The MIT License
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @filesource
+ * @copyright     Copyright 2005-2008, Cake Software Foundation, Inc. (http://www.cakefoundation.org)
+ * @link          http://www.cakefoundation.org/projects/info/cakephp CakePHP(tm) Project
+ * @package       cake
+ * @subpackage    cake.app.config
+ * @since         CakePHP(tm) v 1.1.11.4062
+ * @version       $Revision: 8120 $
+ * @modifiedby    $LastChangedBy: gwoo $
+ * @lastmodified  $Date: 2009-03-19 13:25:10 -0700 (Thu, 19 Mar 2009) $
+ * @license       http://www.opensource.org/licenses/mit-license.php The MIT License
+ */
+return $config['Cake.version'] = '1.2.2.8120';
+?>
