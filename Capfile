b114db57243e600b93308ec18a3403211eb9d22d
Sebastián Gamboa (SagMor) <me@sagmor.com>
2009-04-13 16:22:23 -0400

    Cambios al layout

---
+++ Capfile
@@ -0,0 +1,9 @@
+load 'deploy' if respond_to?(:namespace) # cap2 differentiator
+#Dir['vendor/plugins/*/recipes/*.rb'].each { |plugin| load(plugin) }
+load 'app/config/deploy'
+
+namespace :deploy do
+  task :restart do
+    
+  end
+end
---
+++ app/config/deploy.rb
@@ -0,0 +1,22 @@
+set :application, "portalvcs"
+set :user, 'cc51ag2'
+
+default_run_options[:pty] = true
+set :scm, "git"
+set :repository,  "git://github.com/SagMor/portal-videoconferencias.git"
+set :branch, "master"
+set :deploy_via, :remote_cache
+set :deploy_to, '/u/c/cc51ag2/portalvcs'
+set :use_sudo, false
+
+
+# If you aren't deploying to /u/apps/#{application} on the target
+# servers (which is the default), you can specify the actual location
+# via the :deploy_to variable:
+# set :deploy_to, "/var/www/#{application}"
+
+# If you aren't using Subversion to manage your source code, specify
+# your SCM below:
+# set :scm, :subversion
+
+role :app, "anakena.dcc.uchile.cl"
